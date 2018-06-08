e4819988e7e77311d5cf8d2fc9273c0fb9765ada
Mike Hatcher <mike.hatcher@hiveminder.co>
2018-06-08 14:48:03 +0100

    Component: Locale
    * Created markup for page elements

--- wp-content/themes/techtotem/template-parts/acf-components/content-locale.php
+++ wp-content/themes/techtotem/template-parts/acf-components/content-locale.php
@@ -12,34 +12,318 @@
 <h2 class="badge fuschia">Walk Curator</h2>
 
 <!-- MAP CONTAINER -->
-<div class="map-container">
+<aside class="map-container">
 
 	<!-- MAP -->
 	<div class="map">
 		map
+
+		<!-- JOURNEY TIME -->
+		<nav class="journey-time button-group">
+			<button class="button"><i class="icon route_brisk"></i> 7 <abbr title="minutes">mins</abbr></button>
+			<button class="button"><i class="icon route_curious"></i> 15 <abbr title="minutes">mins</abbr></button>
+			<button class="button"><i class="icon route_leisure"></i> 20 <abbr title="minutes">mins</abbr></button>
+		</nav><!-- .journey-time-->
+
 	</div><!-- .map -->
 
-	<!-- JOURNEY TIME -->
-	<nav class="journey-time">
-		journey time
-	</nav><!-- .journey-time-->
+	<!-- MAP KEY -->
+	<div class="map-key grid-x">
 
-	<!-- KEY -->
-	<div class="key">
-		key
+		<div class="cell small-3">
+
+			<h2><img src="<?php echo get_template_directory_uri() . '/img/icons/min/icon-placeholder.svg'; ?>" width="41" height="41" alt=""> Tranquility</h2>
+
+			<ol class="listing">
+
+				<li>Name lorem ipsum dolor sit amet lorem ipsum dolor</li>
+				<li>Name lorem ipsum dolor sit amet lorem ipsum dolor</li>
+
+			</ol>
+
+		</div>
+
+		<div class="cell small-3">
+
+			<h2><img src="<?php echo get_template_directory_uri() . '/img/icons/min/icon-placeholder.svg'; ?>" width="41" height="41" alt=""> Food & Drink</h2>
+
+			<ol class="listing">
+
+				<li>Name lorem ipsum dolor sit amet lorem ipsum dolor</li>
+				<li>Name lorem ipsum dolor sit amet lorem ipsum dolor</li>
+
+			</ol>
+
+		</div>
+
+		<div class="cell small-3">
+
+			<h2><img src="<?php echo get_template_directory_uri() . '/img/icons/min/icon-placeholder.svg'; ?>" width="41" height="41" alt=""> Attractions</h2>
+
+			<ol class="listing">
+
+				<li>Name lorem ipsum dolor sit amet lorem ipsum dolor</li>
+				<li>Name lorem ipsum dolor sit amet lorem ipsum dolor</li>
+
+			</ol>
+
+		</div>
+
+		<div class="cell small-3">
+
+			<h2><img src="<?php echo get_template_directory_uri() . '/img/icons/min/icon-placeholder.svg'; ?>" width="41" height="41" alt=""> Culture</h2>
+
+			<ol class="listing">
+
+				<li>Name lorem ipsum dolor sit amet lorem ipsum dolor</li>
+				<li>Name lorem ipsum dolor sit amet lorem ipsum dolor</li>
+
+			</ol>
+
+		</div>
+
 	</div><!-- .key -->
 
-</div><!-- .map-container -->
+</aside><!-- .map-container -->
 
-<!-- WEATHER -->
-<div class="weather">
-	weather
-</div><!-- .weather -->
+<!-- CALLOUT CONTAINER -->
+<div class="callout-container">
 
-<!-- EVENTS -->
-<div class="events">
-	events
-</div><!-- .events -->
+	<!-- WEATHER -->
+	<aside class="weather callout yellow">
+		<h2>1 hour forecast</h2>
+		<p>
+			<img src="<?php echo get_template_directory_uri() . '/img/icons/min/icon-placeholder.svg'; ?>" width="175" height="195" alt=""><br>
+			XX% chance of rain
+		</p>
+	</aside><!-- .weather -->
 
+	<!-- EVENTS -->
+	<div class="events callout gray">
+		<h2>
+			<img src="<?php echo get_template_directory_uri() . '/img/icons/min/icon-placeholder.svg'; ?>" width="65" height="65" alt="">
+			Local Events
+		</h2>
 
+		<!-- ORBIT -->
+		<div class="orbit orbit-small" role="region" aria-label="Local events" data-orbit>
 
+			<!-- ORBIT WRAPPER -->
+			<div class="orbit-wrapper">
+
+				<!-- ORBIT CONTROLS -->
+				<div class="orbit-controls">
+
+					<button class="orbit-previous"><img src="<?php echo get_template_directory_uri() . '/img/icons/min/icon-placeholder.svg'; ?>" width="20" height="20" alt=""></button>
+					<button class="orbit-next"><img src="<?php echo get_template_directory_uri() . '/img/icons/min/icon-placeholder.svg'; ?>" width="20" height="20" alt=""></button>
+
+				</div><!-- .orbit-controls -->
+
+				<!-- ORBIT CONTAINER -->
+				<ul class="orbit-container">
+
+					<!-- ORBIT SLIDE -->
+					<li class="orbit-slide">
+
+						<ol class="listing">
+							<li>
+
+								<aside>
+
+									<header>
+
+										<h1>Name lorem ipsum dolor sit amet lorem ipsum dolor amet lorem ipsum dolor</h1>
+
+										<p class="meta">Tag; tag</p>
+
+									</header>
+
+									<div>
+										<p>
+											First line of address only which may end up over two lines<br>
+											Postcode
+										</p>
+									</div>
+
+									<footer>
+
+										<p>XX:XX - XX:XX</p>
+
+										<p>Cost</p>
+
+									</footer>
+
+								</aside>
+								
+							</li>
+							<li>
+
+								<aside>
+
+									<header>
+
+										<h1>Name lorem ipsum dolor sit amet lorem ipsum dolor amet lorem ipsum dolor</h1>
+
+										<p class="meta">Tag; tag</p>
+
+									</header>
+
+									<div>
+										<p>
+											First line of address only which may end up over two lines<br>
+											Postcode
+										</p>
+									</div>
+
+									<footer>
+
+										<p>XX:XX - XX:XX</p>
+
+										<p>Cost</p>
+
+									</footer>
+
+								</aside>
+								
+							</li>
+							<li>
+
+								<aside>
+
+									<header>
+
+										<h1>Name lorem ipsum dolor sit amet lorem ipsum dolor amet lorem ipsum dolor</h1>
+
+										<p class="meta">Tag; tag</p>
+
+									</header>
+
+									<div>
+										<p>
+											First line of address only which may end up over two lines<br>
+											Postcode
+										</p>
+									</div>
+
+									<footer>
+
+										<p>XX:XX - XX:XX</p>
+
+										<p>Cost</p>
+
+									</footer>
+
+								</aside>
+								
+							</li>
+						</ol>
+
+					</li><!-- .orbit-slide -->
+
+					<!-- ORBIT SLIDE -->
+					<li class="orbit-slide">
+
+						<ol class="listing">
+							<li>
+
+								<aside>
+
+									<header>
+
+										<h1>Name lorem ipsum dolor sit amet lorem ipsum dolor amet lorem ipsum dolor</h1>
+
+										<p class="meta">Tag; tag</p>
+
+									</header>
+
+									<div>
+										<p>
+											First line of address only which may end up over two lines<br>
+											Postcode
+										</p>
+									</div>
+
+									<footer>
+
+										<p>XX:XX - XX:XX</p>
+
+										<p>Cost</p>
+
+									</footer>
+
+								</aside>
+								
+							</li>
+							<li>
+
+								<aside>
+
+									<header>
+
+										<h1>Name lorem ipsum dolor sit amet lorem ipsum dolor amet lorem ipsum dolor</h1>
+
+										<p class="meta">Tag; tag</p>
+
+									</header>
+
+									<div>
+										<p>
+											First line of address only which may end up over two lines<br>
+											Postcode
+										</p>
+									</div>
+
+									<footer>
+
+										<p>XX:XX - XX:XX</p>
+
+										<p>Cost</p>
+
+									</footer>
+
+								</aside>
+								
+							</li>
+							<li>
+
+								<aside>
+
+									<header>
+
+										<h1>Name lorem ipsum dolor sit amet lorem ipsum dolor amet lorem ipsum dolor</h1>
+
+										<p class="meta">Tag; tag</p>
+
+									</header>
+
+									<div>
+										<p>
+											First line of address only which may end up over two lines<br>
+											Postcode
+										</p>
+									</div>
+
+									<footer>
+
+										<p>XX:XX - XX:XX</p>
+
+										<p>Cost</p>
+
+									</footer>
+
+								</aside>
+								
+							</li>
+						</ol>
+
+					</li><!-- .orbit-slide -->
+
+				</ul><!-- .orbit-container -->
+
+			</div><!-- .orbit-wrapper -->
+
+		</div><!-- .orbit -->
+
+	</div><!-- .events -->
+
+</div><!-- .callout-container -->
