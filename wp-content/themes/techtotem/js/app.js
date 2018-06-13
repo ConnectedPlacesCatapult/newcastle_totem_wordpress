b85bac62377a8b54dded211b25ddea47bfafa396
Mike Hatcher <mike.hatcher@hiveminder.co>
2018-06-13 15:08:15 +0100

    JS
    * Created function for setting height of screensaver slider

--- wp-content/themes/techtotem/js/app.js
+++ wp-content/themes/techtotem/js/app.js
@@ -1,47 +1,102 @@
 jQuery(function($) {
 
-	/* INIT FOUNDATION */
+
+	/* 
+	 * *******************************************************************************
+	 * FOUNDATION 
+	 * *******************************************************************************
+	 */
 	$(document).foundation();
 
 
-	/* CLOCK */	
-	function tt_getdate(){
+	/* 
+	 * *******************************************************************************
+	 * LIBRARY
+	 * *******************************************************************************
+	 */
 
-		// get date and create vars for hrs, mins
-		var today = new Date();
-		var h = today.getHours();
-		var m = today.getMinutes();
 
-		// add a leading zero to mins
-		if(m<10){
-			m = "0"+m;
+		/* CLOCK */	
+		function tt_get_date(){
+
+			// get date and create vars for hrs, mins
+			var today = new Date();
+			var h = today.getHours();
+			var m = today.getMinutes();
+
+			// add a leading zero to mins
+			if(m<10){
+				m = "0"+m;
+			}
+
+			// create the clock
+			$( '#clock' ).text( h + ":" + m );
+
+			// how often to update the clock
+			setTimeout(function(){
+				tt_get_date();
+			}, 500);
 		}
 
-		// create the clock
-		$( '#clock' ).text( h + ":" + m );
 
-		// how often to update the clock
-		setTimeout(function(){
-			tt_getdate()
-		}, 500);
-	}
+		/* NAVBAR */
+		function tt_content_bottom_spacing() {
+			var contentBottomSpacing = $( '.navbar' ).outerHeight();
+			$('.content').css( 'margin-bottom', contentBottomSpacing );
+		}
 
 
-	/* NAVBAR */
-	function tt_contentBottomSpacing() {
-		var contentBottomSpacing = $( '.navbar' ).innerHeight();
-		$('.content-main').css( 'margin-bottom', contentBottomSpacing );
-	}
-	
+		/* SCREENSAVER */
+		function tt_slider_large() {
+
+			var heightNavbar = $( '.navbar' ).outerHeight();
+			//console.log(heightNavbar);
+
+			var heightWrapper = $( '.wrapper' ).outerHeight();
+			//onsole.log(heightWrapper);
+
+			var heightContent = heightWrapper - heightNavbar;
+			$( '.page-screensaver .content, .page-screensaver .orbit-container' ).height( heightContent);
+		}
+
+
+		/* MAP */
+		function tt_map() {
+			var mapHeight = $( '.map' ).innerHeight();
+			$('#map').height( mapHeight );
+		}
+
+
+		/* JSON DATA */
+		/*function tt_json_data() {
+
+			$.getJSON(  'https://s3-eu-west-1.amazonaws.com/southside.tech.totem/recommendation-totem-1.json', function(data) {
+
+				//console.log(data.solar.name);
+
+			});
+		}*/
+
+
 
 	/* 
-	 * DOCUMENT READY
+	 * *******************************************************************************
+	 * INIT
+	 * *******************************************************************************
 	 */
-	$(document).ready(function(){
+	
 
-		tt_getdate();
-		tt_contentBottomSpacing();
+		/* 
+		 * DOCUMENT READY
+		 */
+		$(document).ready(function(){
 
-    });
+			tt_get_date();
+			tt_content_bottom_spacing();
+			tt_slider_large();
+			tt_map();
+			//tt_json_data();
+
+	    });
 
 });
