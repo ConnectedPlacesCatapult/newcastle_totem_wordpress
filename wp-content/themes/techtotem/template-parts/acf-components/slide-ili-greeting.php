8dddcac1714e9d15cef98c727e759349e05bc45e
Mike Hatcher <mike.hatcher@hiveminder.co>
2018-06-21 10:45:45 +0100

    ILI: Slide
    * Moved anchor element

--- wp-content/themes/techtotem/template-parts/acf-components/slide-ili-greeting.php
+++ wp-content/themes/techtotem/template-parts/acf-components/slide-ili-greeting.php
@@ -9,21 +9,18 @@
  */
 ?>
 
-<a href="<?php echo home_url( '/ili/' ); ?>">
 
-	<p class="lead"><?php echo $recom_msg; ?></p>
+<p class="lead"><?php echo $recom_msg; ?></p>
 
-	<div class="greeting-icons">
+<div class="greeting-icons">
 
-		<img src="<?php echo get_template_directory_uri() . '/img/icons/min/map-path.svg'; ?>" width="507" height="173" class="map-path">
+	<img src="<?php echo get_template_directory_uri() . '/img/icons/min/map-path.svg'; ?>" width="507" height="173" class="map-path">
 
-		<div class="cat-time">
-			<img src="<?php echo get_template_directory_uri() . '/img/icons/min/category-' .  $tt_category . '.svg'; ?>" width="210" height="210" class="icon">
-			<p>7 min walk</p>
-		</div>
-
+	<div class="cat-time">
+		<img src="<?php echo get_template_directory_uri() . '/img/icons/min/category-' .  $tt_category . '.svg'; ?>" width="210" height="210" class="icon">
+		<p>7 min walk</p>
 	</div>
 
-	<p class="more"><span class="button">More</span></p>
+</div>
 
-</a>
+<p class="more"><a href="<?php echo home_url( '/ili/' ); ?>" class="button">More</a></p>
